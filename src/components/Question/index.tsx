b905a8083a0f75da762ebc568034be503deedf13
vitoragp <np.shinigami@gmail.com>
2021-07-15 08:14:18 -0300

    Refatorado o component question, actions foram extraidas como um component a parte

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 
-import { useAuth } from "../../hooks/Auth";
 import { Question as QuestionModel } from "../../models/Question";
 import { Action } from "./Action";
 import { QuestionContextProvider } from "./Context";
@@ -22,47 +21,6 @@ type QuestionProps = {
  */
 
 function Question(props: QuestionProps) {
-  const auth = useAuth();
-  const [pending, setPending] = React.useState<boolean>(false);
-  const [likeCount, setLikeCount] = React.useState<number>(
-    props.data.likeCount
-  );
-
-  /*
-  async function handleLike() {
-    if (props.data.authorId === auth.user.id || pending) {
-      return;
-    }
-    setPending(true);
-
-    let updatedQuestion: QuestionModel;
-    const questionRepository = new QuestionRepository(props.roomCode);
-    const likeRepository = new LikeRepository(props.roomCode, props.data.id);
-    const likes = await likeRepository.getAll();
-    const like = likes?.filter((like) => like.authorId === auth.user?.id);
-
-    if (like?.length) {
-      await likeRepository.delete(like[0]);
-
-      updatedQuestion = await questionRepository.update({
-        ...props.data,
-        likeCount: likeCount - 1,
-      });
-    } else {
-      await likeRepository.create({
-        authorId: auth.user.id,
-      });
-
-      updatedQuestion = await questionRepository.update({
-        ...props.data,
-        likeCount: likeCount + 1,
-      });
-    }
-    setLikeCount(updatedQuestion.likeCount);
-    setPending(false);
-  }
-  */
-
   return (
     <div className="question__component">
       <div className="body">{props.data.body}</div>
